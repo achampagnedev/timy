e328ef83081a1837aa78d925179349e0b5b0378a
Alexandre Champagne <achampagnedev@gmail.com>
2021-04-09 22:54:33 +0800

    Basic timer implemented

--- App.tsx
+++ App.tsx
@@ -1,21 +1,248 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {
+  Vibration,
+  StatusBar,
+  Easing,
+  TextInput,
+  Dimensions,
+  Animated,
+  TouchableOpacity,
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+} from "react-native";
+import { useCallback, useEffect, useRef, useState } from "react";
+const { width, height } = Dimensions.get("window");
+const colors = {
+  background: "#FFFFFF",
+  accent: "#ED1C24",
+  text: "#000000",
+};
 
-export default function App() {
+const timers: number[] = [...Array(13).keys()].map((i) =>
+  i === 0 ? 1 : i * 5
+);
+const ITEM_SIZE: number = width * 0.38;
+const ITEM_SPACING: number = (width - ITEM_SIZE) / 2;
+
+const App = () => {
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const buttonAnimation = useRef(new Animated.Value(0)).current;
+  const timerAnimation = useRef(new Animated.Value(height)).current;
+  const countdownAnimation = useRef(new Animated.Value(timers[0])).current;
+  const countdownRef = useRef();
+  const [duration, setDuration] = useState(timers[0]);
+
+  useEffect(() => {
+    const listener = countdownAnimation.addListener(({ value }) => {
+      countdownRef?.current?.setNativeProps({
+        text: Math.ceil(value).toString(),
+      });
+    });
+
+    return () => {
+      countdownAnimation.removeListener(listener);
+      countdownAnimation.removeAllListeners();
+    };
+  });
+
+  const animation = useCallback(() => {
+    countdownAnimation.setValue(duration);
+
+    Animated.sequence([
+      Animated.timing(buttonAnimation, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+      Animated.timing(timerAnimation, {
+        toValue: 0,
+        duration: 0,
+        useNativeDriver: true,
+      }),
+      Animated.parallel([
+        Animated.timing(countdownAnimation, {
+          toValue: 0,
+          duration: duration * 1000,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        }),
+        Animated.timing(timerAnimation, {
+          toValue: height,
+          duration: duration * 1000,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        }),
+      ]),
+      Animated.delay(1000),
+    ]).start(() => {
+      Vibration.cancel();
+      Vibration.vibrate();
+      countdownAnimation.setValue(duration);
+
+      Animated.timing(buttonAnimation, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start(() => {});
+    });
+  }, [duration]);
+
+  const buttonOpacity = buttonAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0],
+  });
+
+  const buttonTranslateY = buttonAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 200],
+  });
+
+  const textCoundownOpacity = buttonAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 1],
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden />
+      <Animated.View
+        style={[
+          StyleSheet.absoluteFillObject,
+          {
+            height,
+            width,
+            backgroundColor: colors.accent,
+            transform: [
+              {
+                translateY: timerAnimation,
+              },
+            ],
+          },
+        ]}
+      />
+      <Animated.View
+        style={[
+          StyleSheet.absoluteFillObject,
+          {
+            justifyContent: "flex-end",
+            alignItems: "center",
+            paddingBottom: 100,
+            opacity: buttonOpacity,
+            transform: [
+              {
+                translateY: buttonTranslateY,
+              },
+            ],
+          },
+        ]}
+      >
+        <TouchableOpacity onPress={animation}>
+          <View style={styles.roundButton} />
+        </TouchableOpacity>
+      </Animated.View>
+      <View
+        style={{
+          position: "absolute",
+          top: height / 3,
+          left: 0,
+          right: 0,
+          flex: 1,
+        }}
+      >
+        <Animated.View
+          style={{
+            position: "absolute",
+            width,
+            justifyContent: "center",
+            alignSelf: "center",
+            alignItems: "center",
+            opacity: textCoundownOpacity,
+          }}
+        >
+          <TextInput
+            style={styles.text}
+            ref={countdownRef}
+            defaultValue={duration.toString()}
+          />
+        </Animated.View>
+        <Animated.FlatList
+          data={timers}
+          keyExtractor={(item) => item.toString()}
+          horizontal
+          bounces={false}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            { useNativeDriver: true }
+          )}
+          onMomentumScrollEnd={(ev) => {
+            const index = Math.round(
+              ev.nativeEvent.contentOffset.x / ITEM_SIZE
+            );
+            setDuration(timers[index]);
+          }}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: ITEM_SPACING }}
+          snapToInterval={ITEM_SIZE}
+          decelerationRate="fast"
+          style={{ flexGrow: 0, opacity: buttonOpacity }}
+          renderItem={({ item, index }) => {
+            const inputRange = [
+              (index - 1) * ITEM_SIZE,
+              index * ITEM_SIZE,
+              (index + 1) * ITEM_SIZE,
+            ];
+
+            const opacity = scrollX.interpolate({
+              inputRange,
+              outputRange: [0.4, 1, 0.4],
+            });
+
+            const scale = scrollX.interpolate({
+              inputRange,
+              outputRange: [0.6, 1, 0.6],
+            });
+
+            return (
+              <View
+                style={{
+                  width: ITEM_SIZE,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Animated.Text
+                  style={[styles.text, { opacity, transform: [{ scale }] }]}
+                >
+                  {item}
+                </Animated.Text>
+              </View>
+            );
+          }}
+        />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.background,
+  },
+  roundButton: {
+    width: 80,
+    height: 80,
+    borderRadius: 80,
+    backgroundColor: colors.accent,
+  },
+  text: {
+    fontSize: ITEM_SIZE * 0.8,
+    // fontFamily: "Menlo",
+    color: colors.text,
+    fontWeight: "900",
   },
 });
+
+export default App;
